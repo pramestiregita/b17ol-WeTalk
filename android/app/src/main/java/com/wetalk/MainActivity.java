04b27fa0fc22ef5ea07e0654c1c9161a224ce189
prmstrgt <pramestiregitaa@gmail.com>
2020-11-26 05:59:28 +0700

    change app name

--- android/app/src/main/java/com/wetalk/MainActivity.java
+++ android/app/src/main/java/com/wetalk/MainActivity.java
@@ -1,4 +1,4 @@
-package com.chatapp;
+package com.wetalk;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "chatapp";
+    return "WeTalk";
   }
 }
--- ios/WeTalkTests/WeTalkTests.m
+++ ios/WeTalkTests/WeTalkTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface chatappTests : XCTestCase
+@interface WeTalkTests : XCTestCase
 
 @end
 
-@implementation chatappTests
+@implementation WeTalkTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
