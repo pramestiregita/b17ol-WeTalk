d526af523534aa7d9f5a99ca90733a6991a445a3
prmstrgt <pramestiregitaa@gmail.com>
2020-12-12 06:03:34 +0700

    edit styling

--- src/components/ChatList/index.js
+++ src/components/ChatList/index.js
@@ -2,16 +2,7 @@ import React from 'react';
 import {useDispatch, useSelector} from 'react-redux';
 import {useNavigation} from '@react-navigation/native';
 import {StyleSheet, TouchableOpacity} from 'react-native';
-import {
-  Thumbnail,
-  Text,
-  List,
-  ListItem,
-  Left,
-  Body,
-  Right,
-  View,
-} from 'native-base';
+import {Thumbnail, Text, View} from 'native-base';
 import moment from 'moment';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
@@ -37,9 +28,9 @@ export default function ChatList({item}) {
   };
 
   return (
-    <List key={item.id}>
-      <ListItem onPress={() => seeDetails()} avatar>
-        <Left>
+    <View style={styled.parent}>
+      <TouchableOpacity onPress={() => seeDetails()}>
+        <View style={styled.list}>
           <TouchableOpacity>
             <Thumbnail
               style={styled.image}
@@ -54,41 +45,76 @@ export default function ChatList({item}) {
               }
             />
           </TouchableOpacity>
-        </Left>
 
-        <Body>
-          {item.sender.id !== userId ? (
-            <Text style={styled.name}>{item.sender.name}</Text>
-          ) : (
-            <Text style={styled.name}>{item.recipient.name}</Text>
-          )}
-          <Text style={styled.text}>
-            {item.content.length > 30 ? item.preview : item.content}
-          </Text>
-        </Body>
-
-        <Right style={styled.info}>
-          <Text note>{moment(item.createdAt).format('hh:mm A')}</Text>
-          {item.sender.id === userId ? (
-            item.isRead ? (
-              <Icon name="done-all" size={20} color={color.linkColor} />
-            ) : (
-              <Icon name="done-all" size={20} color="grey" />
-            )
-          ) : !item.isRead ? (
-            <View style={styled.badge}>
-              <Text />
+          <View style={styled.devider}>
+            <View style={styled.body}>
+              {item.sender.id !== userId ? (
+                <Text style={styled.name}>{item.sender.name}</Text>
+              ) : (
+                <Text style={styled.name}>{item.recipient.name}</Text>
+              )}
+              <Text style={styled.text}>
+                {item.content.length > 30 ? item.preview : item.content}
+              </Text>
             </View>
-          ) : null}
-        </Right>
-      </ListItem>
-    </List>
+
+            <View style={styled.info}>
+              <Text note>{moment(item.createdAt).format('hh:mm A')}</Text>
+              {item.sender.id === userId ? (
+                item.isRead ? (
+                  <Icon
+                    style={styled.icon}
+                    name="done-all"
+                    size={20}
+                    color={color.linkColor}
+                  />
+                ) : (
+                  <Icon
+                    style={styled.icon}
+                    name="done-all"
+                    size={20}
+                    color="grey"
+                  />
+                )
+              ) : !item.isRead ? (
+                <View style={styled.badge}>
+                  <Text />
+                </View>
+              ) : null}
+            </View>
+          </View>
+        </View>
+      </TouchableOpacity>
+    </View>
   );
 }
 
 const styled = StyleSheet.create({
+  parent: {
+    paddingHorizontal: 10,
+    marginTop: 5,
+    height: 65,
+    justifyContent: 'center',
+  },
+  list: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
   image: {
     justifyContent: 'center',
+  },
+  devider: {
+    flex: 1,
+    flexDirection: 'row',
+    borderBottomColor: 'grey',
+    borderBottomWidth: 0.5,
+    height: '100%',
+    alignItems: 'center',
+  },
+  body: {
+    flex: 1,
+    marginRight: 5,
+    marginLeft: 10,
   },
   name: {
     fontWeight: 'bold',
@@ -97,12 +123,16 @@ const styled = StyleSheet.create({
     fontSize: 13,
   },
   info: {
-    justifyContent: 'space-between',
+    alignItems: 'flex-end',
+  },
+  icon: {
+    marginTop: 5,
   },
   badge: {
     backgroundColor: color.btnColor,
-    width: 20,
-    height: 20,
-    borderRadius: 10,
+    width: 14,
+    height: 14,
+    borderRadius: 7,
+    marginTop: 5,
   },
 });
